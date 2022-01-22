8f16ee95df8e9ac98df5817c49e17dc0b5d99c05
Vítor Marçal <git@vitormarcal.com.br>
2022-01-22 17:24:11 -0300

    adiciona execute que recebe lista

--- src/main/kotlin/br/com/vitormarcal/investimentos/usecase/CalculatesTickerAveragePriceUseCase.kt
+++ src/main/kotlin/br/com/vitormarcal/investimentos/usecase/CalculatesTickerAveragePriceUseCase.kt
@@ -2,6 +2,7 @@ package br.com.vitormarcal.investimentos.usecase
 
 import br.com.vitormarcal.investimentos.input.usecase.CalculatesTickerAveragePrice
 import br.com.vitormarcal.investimentos.output.TickerAveragePriceOutput
+import br.com.vitormarcal.investimentos.output.TradeOutput
 import br.com.vitormarcal.investimentos.repository.TradeRepository
 import org.springframework.stereotype.Service
 
@@ -9,9 +10,14 @@ import org.springframework.stereotype.Service
 class CalculatesTickerAveragePriceUseCase(
     private val tradeRepository: TradeRepository
 ): CalculatesTickerAveragePrice {
+
     override fun execute(ticker: String): TickerAveragePriceOutput {
-        return tradeRepository.findByTicker(ticker)
-            .fold(TickerAveragePriceOutput(), TickerAveragePriceOutput::add)
+        return execute(tradeRepository.findByTicker(ticker).toList())
+
+    }
+
+    override fun execute(tradeList: List<TradeOutput>): TickerAveragePriceOutput {
+        return tradeList.fold(TickerAveragePriceOutput(), TickerAveragePriceOutput::add)
     }
 
 

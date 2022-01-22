19f93c5b6b0ff1856523d1a58bdf098c5efd6090
Vítor Marçal <vitormarcal8@gmail.com>
2022-01-22 17:24:11 -0300

    adiciona execute que recebe lista

--- src/main/kotlin/br/com/vitormarcal/investimentos/input/usecase/CalculatesTickerAveragePrice.kt
+++ src/main/kotlin/br/com/vitormarcal/investimentos/input/usecase/CalculatesTickerAveragePrice.kt
@@ -1,8 +1,10 @@
 package br.com.vitormarcal.investimentos.input.usecase
 
 import br.com.vitormarcal.investimentos.output.TickerAveragePriceOutput
+import br.com.vitormarcal.investimentos.output.TradeOutput
 
 interface CalculatesTickerAveragePrice {
 
     fun execute(ticker: String): TickerAveragePriceOutput
+    fun execute(tradeList: List<TradeOutput>): TickerAveragePriceOutput
 }
